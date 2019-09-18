c349dcd5b7d610932a8b81f41258b34d3c95a669
ThibaultVlieghe <thibault.vlieghe@mcgill.ca>
2019-09-18 14:15:18 -0400

    modified analysis/ python files for better readability and flexibility, still only handles day one learning

--- analysis/ld_analysis_fromScratch.py
+++ analysis/ld_analysis_fromScratch.py
@@ -1,16 +1,11 @@
-import sys
 import os
 import csv
-import numpy as np
-import re
-import unicodedata
-from scipy.spatial import distance
+import sys
 
 from expyriment.misc import data_preprocessing
 
 # from config import matrix_size
-from ld_utils_fromScratch import extract_correct_answers
-from ld_utils_fromScratch import Days
+from ld_utils_fromScratch import day, extract_matrix_and_data, extract_events, write_csv
 
 # ***INSTRUCTIONS***
 # Please input the location of the subject folder containing the data you wish to convert to .csv format
@@ -22,18 +17,18 @@ from ld_utils_fromScratch import Days
 # In this example, Output will be:  <subject_41.csv> <DeCoNap/data/subject_41.csv>
 
 sep = os.path.sep
-
-# Declaring <subjectFolder> and <outputFile> variables, which are self-explanatory
-# subjectLocation = sys.argv[1]
-subjectLocation = '../data/Latifa_early_data_reorganized/HBHL_Dec48_NO_004/'
-if subjectLocation[-1] == sep:  # removing path separator if not present. e.g. <data/> to <data>
-    subjectLocation = subjectLocation[:-1]
-subjectFolder = os.getcwd() + sep + subjectLocation + sep
-outputFileLearning = os.getcwd() + sep + subjectLocation + '_learning.csv'
-outputFileTests = os.getcwd() + sep + subjectLocation + '_tests&recognition.csv'
+#
+# Declaring <subject_folder> and <outputFile> variables, which are self-explanatory
+# subject_location = sys.argv[1]
+subject_location = '../../DeCoNap-HBHL_DT48_v0.3/data/Latifa_early_data_reorganized/HBHL_Dec48_NO_004/'
+if subject_location[-1] == sep:  # removing path separator if not present. e.g. <data/> to <data>
+    subject_location = subject_location[:-1]
+subject_folder = os.getcwd() + sep + subject_location + sep
+output_file_learning = os.getcwd() + sep + subject_location + '_learning.csv'
+outputFileTests = os.getcwd() + sep + subject_location + '_tests&recognition.csv'
 
 # Gathering all subject files
-allFiles = os.listdir(subjectFolder)
+allFiles = os.listdir(subject_folder)
 declarativeFiles = []
 for iFile in allFiles:
     # There is only one recognition test (and therefore only one file) in our research paradigm
@@ -42,102 +37,36 @@ for iFile in allFiles:
     elif 'ld_declarativeTask' in iFile:
         declarativeFiles.append(iFile)
 
-isInterference = False
-dayOneTestLearning = Days()
-dayTwoTestLearning = Days()
-dayThreeTestLearning = Days()
-dayThreeRecognition = Days()
+day1_learning = day()
+day2_test = day()
+day3_test = day()
+day3_recognition = day()
 
 for iFile in allFiles:
-    header = data_preprocessing.read_datafile(subjectFolder + iFile, only_header_and_variable_names=True)
+    header = data_preprocessing.read_datafile(subject_folder + iFile, only_header_and_variable_names=True)
     header[3].split('\n#e ')
     for field in header:
         if "DayOne-Learning" in field and "Experiment" in field:
-            dayOneTestLearning.name = 'DayOne-TestLearning'
-            dayOneTestLearning.header = header
-            dayOneTestLearning.ifile = iFile
-            dayOneTestLearning.matrix, dayOneTestLearning.data, \
-            dayOneTestLearning.correctCards, dayOneTestLearning.wrongCards, number_blocks \
-                = extract_correct_answers(subjectFolder, iFile)
+            events, matrix_pictures, matrix_size = extract_matrix_and_data(subject_folder, iFile)
+            cards_order, cards_distance_to_correct_card, number_blocks = extract_events(events, matrix_size)
+            write_csv(output_file_learning, matrix_pictures, cards_order, cards_distance_to_correct_card, number_blocks)
             break
     for field in header:
         if "DayOne-TestLearning" in field and "Experiment" in field:
-            dayTwoTestLearning.name = 'DayOne-TestLearning'
-            dayTwoTestLearning.header = header
-            dayTwoTestLearning.ifile = iFile
-            dayTwoTestLearning.matrix, dayOneTestLearning.data, \
-            dayTwoTestLearning.correctCards, dayOneTestLearning.wrongCards, number_blocks \
-                = extract_correct_answers(subjectFolder, iFile)
+            # dayTwoTestLearning.header = header
+            # dayTwoTestLearning.matrix = extract_matrix(subject_folder, iFile)
             break
-
-if len(dayOneTestLearning.matrix) > 36:
-    matrix_size = (7, 7)
-elif len(dayOneTestLearning.matrix) < 35:
-    matrix_size = (5, 5)
-else:
-    matrix_size = (6, 6)
+    for field in header:
+        if "DayOne-TestLearning" in field and "Experiment" in field:
+            # dayThreeTestLearning.header = header
+            # dayThreeTestLearning.matrix = extract_matrix(subject_folder, iFile)
+            break
 
 # as a rule of thumb, for 'DayRec_MatrixA_answer' and 'DayRec_matrixRec_answer', remember that
 # 0 means "the subject made a mistake"
 # 1 means "the subject got it right"
 # 1 in 'DayRec_matrixRec_answer' means the subject clicked "Wrong" when presented with the wrong position. And 0, that
 # they were mistaken
-iCSV = csv.writer(open(outputFileLearning, "wb"))
-firstRow = ['Item', 'Class']
-for i in range(number_blocks):
-    firstRow.extend(['Day1_Block' + str(i) + '_matrixA_order', 'Day1_Block' + str(i) + '_matrixA_distanceToMatrixA'])
-iCSV.writerow(firstRow)
-
-# card_events in DayOneTest
-events = dayOneTestLearning.header[-1].split('\n')
-events = [element.encode('ascii') for element in events]
-cards_position = []
-cards_distanceToCorrectCard = []
-cards_order = []
-
-for event in events:
-    if 'Block' in event and 'Test' in event:
-        cards_position.append({})
-        cards_distanceToCorrectCard.append({})
-        cards_order.append({})
-        block_number = len(cards_position) - 1
-        register_on = True
-        order = 0
-    elif 'Block' in event and 'Presentation' in event:
-        register_on = False
-    elif 'ShowCueCard' in event and register_on:
-        card = re.search('(?<=card_)\w+', event).group(0)
-        position = cards_position[block_number][card] = re.search('pos_([0-9]+)_', event).group(1)
-        cards_order[block_number][card] = order
-        order += 1
-        cards_distanceToCorrectCard[block_number][card] = 'NaN'
-    elif 'Response' in event and 'NoResponse' not in event and 'pos_None_ERROR' not in event and register_on:
-        response = re.search('(?<=card_)\w+', event).group(0)
-        if response == card:
-            cards_distanceToCorrectCard[block_number][card] = 0
-        else:
-            response_position = re.search('pos_([0-9]+)_', event).group(1)
-            cards_distanceToCorrectCard[block_number][card] = distance.euclidean(
-                np.unravel_index(int(position), matrix_size),
-                np.unravel_index(int(response_position), matrix_size))
-
-assert number_blocks == block_number+1, "Two methods for block counting don't output the same result," \
-                                      " please check the log files"
-
-for card in np.sort(dayOneTestLearning.matrix):
-    # Add item; Add category
-    card = card.rstrip('.png')
-    itemList = [card, card[0]]
-    for block_number in range(number_blocks):
-        try:
-            itemList.extend([cards_order[block_number][card], cards_distanceToCorrectCard[block_number][card]])
-        except KeyError:
-            itemList.extend(['NaN', 'NaN'])
-    iCSV.writerow(itemList)
-
-# DAY 2 ################################################################################################################
-
-
 
 iCSV = csv.writer(open(outputFileTests, "wb"))
 iCSV.writerow(['Item', 'Class',
--- analysis/ld_utils_fromScratch.py
+++ analysis/ld_utils_fromScratch.py
@@ -1,3 +1,5 @@
+import csv
+
 import numpy as np
 import ast
 import re
@@ -20,16 +22,87 @@ class WrongCards(object):
         self.picture = []
 
 
-class Days(object):
-
+class day(object):
     def __init__(self):
-        self.name = ''
-        self.header = ''
-        self.ifile = ''
-        self.data = []
-        self.correctCards = CorrectCards()
-        self.wrongCards = WrongCards()
         self.matrix = []
+        self.header = []
+
+
+def extract_matrix_and_data(i_folder, i_file):
+    header = data_preprocessing.read_datafile(i_folder + i_file, only_header_and_variable_names=True)
+
+    # Extracting pictures' positions in the matrix
+    header2 = header[3].split('\n#e ')
+    matrix_pictures = ast.literal_eval(header2[header2.index('Positions pictures:') + 1].split('\n')[0].split('\n')[0])
+    matrix_pictures = [element for element in matrix_pictures if element is not None]
+
+    # Extracting data
+    events = header[-1].split('\n')
+    events = [element.encode('ascii') for element in events]
+
+    if len(matrix_pictures) == 48:
+        matrix_size = (7, 7)
+    elif len(matrix_pictures) == 36:
+        matrix_size = (6, 6)
+    elif len(matrix_pictures) == 25:
+        matrix_size = (5, 5)
+    else:
+        raise ValueError('Matrix dimensions cannot be identified')
+
+    return events, matrix_pictures, matrix_size
+
+
+def extract_events(events, matrix_size):
+    cards_position = []
+    cards_distance_to_correct_card = []
+    cards_order = []
+    for event in events:
+        if 'Block' in event and 'Test' in event:
+            cards_position.append({})
+            cards_distance_to_correct_card.append({})
+            cards_order.append({})
+            block_number = len(cards_position) - 1
+            register_on = True
+            order = 0
+        elif 'Block' in event and 'Presentation' in event:
+            register_on = False
+        elif 'ShowCueCard' in event and register_on:
+            card = re.search('(?<=card_)\w+', event).group(0)
+            position = cards_position[block_number][card] = re.search('pos_([0-9]+)_', event).group(1)
+            cards_order[block_number][card] = order
+            order += 1
+            cards_distance_to_correct_card[block_number][card] = 'NaN'
+        elif 'Response' in event and 'NoResponse' not in event and 'pos_None_ERROR' not in event and register_on:
+            response = re.search('(?<=card_)\w+', event).group(0)
+            if response == card:
+                cards_distance_to_correct_card[block_number][card] = 0
+            else:
+                response_position = re.search('pos_([0-9]+)_', event).group(1)
+                cards_distance_to_correct_card[block_number][card] = distance.euclidean(
+                    np.unravel_index(int(position), matrix_size),
+                    np.unravel_index(int(response_position), matrix_size))
+
+    return cards_order, cards_distance_to_correct_card, block_number+1
+
+
+def write_csv(output_file_learning, matrix_pictures, cards_order, cards_distance_to_correct_card, number_blocks):
+    i_csv = csv.writer(open(output_file_learning, "wb"))
+    first_row = ['Item', 'Class']
+    for i in range(number_blocks):
+        first_row.extend(
+            ['Day1_Block' + str(i) + '_matrixA_order', 'Day1_Block' + str(i) + '_matrixA_distanceToMatrixA'])
+    i_csv.writerow(first_row)
+
+    for card in np.sort(matrix_pictures):
+        # Add item; Add category
+        card = card.rstrip('.png')
+        item_list = [card, card[0]]
+        for block_number in range(number_blocks):
+            try:
+                item_list.extend([cards_order[block_number][card], cards_distance_to_correct_card[block_number][card]])
+            except KeyError:
+                item_list.extend(['NaN', 'NaN'])
+        i_csv.writerow(item_list)
 
 
 def extract_correct_answers(i_folder, i_file):
@@ -88,19 +161,3 @@ def extract_correct_answers(i_folder, i_file):
         invalid_cards.element = [word for word in invalid_cards.picture if word[0] == element]
 
     return matrix_pictures, data, valid_cards, invalid_cards, len(block_indexes)
-
-# Here the output is the picture that has been asked.
-# invalid_cards.animals = [word for word in invalid_cards.picture if word[0] == 'a']
-# invalid_cards.clothes = [word fo#r word in invalid_cards.picture if word[0] == 'c']
-# invalid_cards.vehicles = [word for word in invalid_cards.picture if word[0] == 'v']
-# invalid_cards.fruits = [word for word in invalid_cards.answer if word[0] == 'f']
-
-# valid_cards.animals = [word for word in valid_cards.answer if word[0] == 'a']
-# valid_cards.clothes = [word for word in valid_cards.answer if word[0] == 'c']
-# valid_cards.vehicles = [word for word in valid_cards.answer if word[0] == 'v']
-# valid_cards.fruits = [word for word in valid_cards.answer if word[0] == 'f']
-
-# self.animals = []
-# self.clothes = []
-# self.vehicles = []
-# self.fruits = []
